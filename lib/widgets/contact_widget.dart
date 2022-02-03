2438541c8285b248eb797429bb51ee7ffc7fa3ba
Antony Ruby Alexia <alexia@m2p.in>
2022-02-03 16:18:43 +0530

    changed portfolio screen alignament for experience

--- lib/widgets/contact_widget.dart
+++ lib/widgets/contact_widget.dart
@@ -6,11 +6,9 @@ import 'package:url_launcher/url_launcher.dart';
 class ExperienceModel {
   final String experienceTitle;
   final String experienceSubTitle;
+  final double? height;
 
-  ExperienceModel(
-    this.experienceTitle,
-    this.experienceSubTitle,
-  );
+  ExperienceModel(this.experienceTitle, this.experienceSubTitle, {this.height});
 }
 
 class Contact {
@@ -31,7 +29,7 @@ class Contact {
           padding: const EdgeInsets.all(28.0),
           child: CircleAvatar(
             backgroundColor: Colors.transparent,
-            radius: 160.0,
+            radius: 150.0,
             child: ClipRRect(
               child: Image.asset(image),
               borderRadius: BorderRadius.circular(150.0),
@@ -49,7 +47,7 @@ class Contact {
         ),
         const SizedBox(height: 60),
         const Text("Introduction",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         Padding(
           padding: const EdgeInsets.all(28.0),
           child: Text(
@@ -59,7 +57,7 @@ class Contact {
         ),
         const SizedBox(height: 40),
         const Text("About Me",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         Padding(
           padding: const EdgeInsets.all(48.0),
           child: Text(
@@ -70,7 +68,7 @@ class Contact {
         const Padding(
           padding: EdgeInsets.only(top: 40.0, bottom: 40),
           child: Text("Experience",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         ),
         Padding(
           padding: const EdgeInsets.only(left: 38.0, right: 20),
@@ -81,8 +79,9 @@ class Contact {
                 shrinkWrap: true,
                 itemBuilder: (BuildContext context, int index) {
                   return DesignWidget(
-                      title: experience[index].experienceTitle,
-                      subTitle: experience[index].experienceSubTitle);
+                    title: experience[index].experienceTitle,
+                    subTitle: experience[index].experienceSubTitle,
+                  );
                 }),
             Container(
                 padding: const EdgeInsets.all(12.0),
@@ -142,20 +141,73 @@ class Contact {
 class DesignWidget extends StatefulWidget {
   final String title;
   final String subTitle;
+  final double height;
 
-  const DesignWidget({Key? key, required this.title, required this.subTitle})
+  const DesignWidget(
+      {Key? key,
+      required this.title,
+      required this.subTitle,
+      this.height = 500})
       : super(key: key);
 
   @override
   _DesignWidgetState createState() => _DesignWidgetState();
 }
 
+// class _DesignWidgetState extends State<DesignWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       children: [
+//         Column(
+//           children: [
+//             Container(
+//                 padding: const EdgeInsets.all(12.0),
+//                 decoration: const BoxDecoration(
+//                     color: Colors.blue,
+//                     borderRadius: BorderRadius.all(Radius.circular(20))),
+//                 child: const Icon(
+//                   Icons.phone_android,
+//                   color: Colors.white,
+//                 )),
+//             const SizedBox(
+//                 height: 300,
+//                 width: 20,
+//                 child: VerticalDivider(color: Colors.blue)),
+//           ],
+//         ),
+//         Expanded(
+//           child: Padding(
+//             padding: const EdgeInsets.only(left: 38.0),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Text(widget.title,
+//                     style: const TextStyle(
+//                       fontSize: 16,
+//                       fontWeight: FontWeight.w600,
+//                     )),
+//                 const SizedBox(height: 20),
+//                 Text(
+//                   widget.subTitle,
+//                 ),
+//                 // const SizedBox(height: 30)
+//               ],
+//             ),
+//           ),
+//         )
+//       ],
+//     );
+//   }
+// }
+
 class _DesignWidgetState extends State<DesignWidget> {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Column(
+        Row(
           children: [
             Container(
                 padding: const EdgeInsets.all(12.0),
@@ -166,32 +218,37 @@ class _DesignWidgetState extends State<DesignWidget> {
                   Icons.phone_android,
                   color: Colors.white,
                 )),
-            const SizedBox(
-                height: 300,
-                width: 20,
-                child: VerticalDivider(color: Colors.blue)),
-          ],
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 38.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(widget.title,
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 38.0, top: 50, bottom: 50),
+                child: Text(widget.title,
                     style: const TextStyle(
                       fontSize: 16,
                       fontWeight: FontWeight.w600,
                     )),
-                const SizedBox(height: 20),
-                Text(
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: SizedBox(
+                  height: widget.height,
+                  width: 10,
+                  child: const VerticalDivider(color: Colors.blue)),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 68.0, right: 20),
+                child: Text(
                   widget.subTitle,
                 ),
-                const SizedBox(height: 30)
-              ],
+              ),
             ),
-          ),
-        )
+          ],
+        ),
       ],
     );
   }
